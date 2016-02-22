28aaa7172e6ebc142e5dbaa54919d90430774aa5
Nick Maguire <ndm2y@virginia.edu>
2016-02-22 11:40:24 -0800

    New branch

--- hello.py
+++ hello.py
@@ -2,6 +2,8 @@ __author__="__ndm2y__"
 #Created by: Nick Maguire
 
 from helper import greeting
+from hurter import leaving
 
 if __name__=="__main__":
     greeting("hello")
+    leaving("goodbye")
